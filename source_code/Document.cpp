bff3713c29fa2004e1dbdccdbefe898d4e66ab56
adamzhang <mingpingzhang@163.com>
2017-11-05 11:58:50 +0800

    fond the links and the text of the links

--- source_code/Document.cpp
+++ source_code/Document.cpp
@@ -5,6 +5,7 @@
 #include <QEventLoop>
 #include <QUrl>
 #include <cassert>
+#include <gumbo.h>
 
 using namespace std;
 
@@ -30,10 +31,41 @@ QString Document::getHtmlContent(const QString& url)
 	return reply->readAll();
 }
 
+static void find_links(map<QString, QString>& pairs, const GumboNode* node)
+{
+	if (node->type != GUMBO_NODE_ELEMENT) 
+		return;
+	if(node->v.element.tag == GUMBO_TAG_A)
+	{
+		GumboAttribute* href = gumbo_get_attribute(&node->v.element.attributes, "href");
+		if (href)
+		{
+			QString link = href->value;
+			QString text = node->v.text.text;
+			qDebug() << "link:" << link << " " << "text:" << text;
+			pairs.emplace(link, text);
+		}
+	}
+	const GumboVector* children = &node->v.element.children;
+	for(auto i = 0; i < children->length; ++i)
+		find_links(pairs, static_cast<GumboNode*>(children->data[i]));
+}
+
+static bool find_links(map<QString, QString>& pairs, const QString& text)
+{
+	GumboOutput* node = gumbo_parse(text.toStdString().c_str());
+	if (!node)
+		return false;
+	find_links(pairs, node->root);
+	gumbo_destroy_output(&kGumboDefaultOptions, node);
+	qDebug() << "pairs' size:" << pairs.size();
+	return true;
+}
+
 static map<QString, QString> parseHtml(const QString& content)
 {
 	map<QString, QString> pairs;
-	qDebug() << content;
+	find_links(pairs, content);
 	return pairs;
 }
 
